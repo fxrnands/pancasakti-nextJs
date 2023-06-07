6ffbaa3673cab6198f5b2d74bd1e79f5b8ff1453
M. Fernanda Kholil Jibron <110803660+fxrnands@users.noreply.github.com>
2023-06-07 23:57:25 +0700

    error build

--- src/pages/components/base/FlyoutButtons.tsx
+++ src/pages/components/base/FlyoutButtons.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 
-export interface ExampleProps {
+export interface ButtonProps {
   list: string[];
   buttonName: string;
 }
 
-export default function Button({ list, buttonName }: ExampleProps) {
+export default function Button({ list, buttonName }: ButtonProps) {
   return (
     <Popover className="relative">
       <Popover.Button className="inline-flex justify-center outline-none w-[15rem] bg-white rounded-[4px] px-2 py-2 items-center text-sm font-semibold leading-6 text-gray-900">
@@ -25,7 +25,7 @@ export default function Button({ list, buttonName }: ExampleProps) {
         <Popover.Panel className="absolute left-1/2 z-10 mt-1 flex w-screen max-w-max -translate-x-1/2 px-4">
           <div className="w-60 max-w-md flex-auto overflow-hidden rounded-[4px] bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
             <div className="p-2">
-              {list.map((item, index) => (
+              {list?.map((item, index) => (
                 <div
                   key={index}
                   className="group relative p-2 flex gap-x-6 rounded-[4px] hover:bg-gray-200"
