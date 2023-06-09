a11d5b93a0d1d0e66b4bb5a5e128f97b54a59abe
M. Fernanda Kholil Jibron <110803660+fxrnands@users.noreply.github.com>
2023-06-09 19:42:16 +0700

    fix whatsapp pop up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Navbar from "./components/base/Navbar";
+import FlyingPopups from "./components/base/FlyingPopups";
 import Footer from "./components/base/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Navbar />
       <Component {...pageProps} />
+      <FlyingPopups />
       <Footer />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,7 +124,6 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <FlyingPopups />
     </>
   );
 }
